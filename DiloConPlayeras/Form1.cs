17d2fac1e6843bfd04673e8de465816d8838d994
brend <brend@BRENDA_QUIROZ>
2024-05-07 23:05:44 -0600

    Cambios button1

--- DiloConPlayeras/Form1.cs
+++ DiloConPlayeras/Form1.cs
@@ -6,5 +6,10 @@ namespace DiloConPlayeras
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
