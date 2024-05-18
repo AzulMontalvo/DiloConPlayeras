3d33eeacf50d538da14315753c9f4e9a5796edb8
brend <brend@BRENDA_QUIROZ>
2024-05-17 22:11:17 -0600

    Se agregaron las acciones de los botones

--- DiloConPlayeras/Form1.cs
+++ DiloConPlayeras/Form1.cs
@@ -66,7 +66,20 @@ namespace DiloConPlayeras
 
         private void btnVentpedidos_Click_1(object sender, EventArgs e)
         {
+            Pedidos pedidos = new Pedidos();
+            pedidos.Show();
+        }
 
+        private void Inventario_Click_1(object sender, EventArgs e)
+        {
+            Inventario inventario = new Inventario();
+            inventario.Show();
+        }
+
+        private void Catalago_Click_1(object sender, EventArgs e)
+        {
+            Catalago catalago=new Catalago();
+            catalago.Show();
         }
     }
 }
